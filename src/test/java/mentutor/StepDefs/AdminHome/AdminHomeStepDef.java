66163116b56c16b4f15c5a11836b0ca5c08445ea
AnjarTiyo <anjar.jog@gmail.com>
2022-11-10 11:55:21 +0700

    add feature: Admin Home Page | Page Title

--- src/test/java/mentutor/StepDefs/AdminHome/AdminHomeStepDef.java
+++ src/test/java/mentutor/StepDefs/AdminHome/AdminHomeStepDef.java
@@ -1,14 +1,24 @@
 package mentutor.StepDefs.AdminHome;
 
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import mentutor.Interactions.UserInteractions;
 import mentutor.Page.AdminHomePage;
+import org.junit.jupiter.api.Assertions;
 
-public class AdminHomeStepDef {
+import static mentutor.Page.AdminHomePage.*;
+
+public class AdminHomeStepDef extends UserInteractions {
 
     AdminHomePage admin;
 
     @And("User click nav option {} on Admin Page")
     public void userClickNavOptionOnAdminPage(String page) {
         admin.clickNavPage(page);
+    }
+
+    @Then("Admin Home Page Title should be {}")
+    public void adminHomePageTitleShouldBeHelloAdmin(String title) {
+        Assertions.assertEquals(title, getElementText(ADMIN_HOME_PAGE_TITLE));
     }
 }
