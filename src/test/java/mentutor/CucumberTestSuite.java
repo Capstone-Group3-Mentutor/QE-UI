75b085e1bd3d0348757f6de56740d528fe8e1cde
AnjarTiyo <anjar.jog@gmail.com>
2022-11-13 13:30:33 +0700

    Manual Testing | Add scenario of manual testing for Mentee Home Page feature

--- src/test/java/mentutor/CucumberTestSuite.java
+++ src/test/java/mentutor/CucumberTestSuite.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"pretty"},
         features = "src/test/resources/features",
-        tags = "@fixing1"
+        tags = "@Manual"
 )
 public class CucumberTestSuite {
     public static final String BASE_URL = "https://mentutor.vercel.app/";
